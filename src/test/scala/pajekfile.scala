d809e1ca592c8075e5d7fbf739a355751074797d
Felix Fung <fylixeoi@gmail.com>
2017-11-03 11:13:14 -0400

    Tidied up code in PajekFile and Nodes.
    Nodal weights are never used, so is now not read in PajekFile,
    nor stored in Nodes.
    
    Took away tests on Nets/kin.net in the unit tests.

--- src/test/scala/pajekfile.scala
+++ src/test/scala/pajekfile.scala
@@ -25,6 +25,7 @@ class PajekFileTest extends FunSuite with BeforeAndAfter
       .setMaster("local[*]")
       .set("spark.default.parallelism", "1")
     sc = new SparkContext(conf)
+    sc.setLogLevel("OFF")
   }
 
   /***************************************************************************
@@ -41,34 +42,7 @@ class PajekFileTest extends FunSuite with BeforeAndAfter
     val pajek = new PajekFile(sc,"Nets/trivial.net")
     assert( pajek.n === 2 )
     assert( pajek.names.collect() === Array((1,"m01"),(2,"m02")) )
-    assert( pajek.weights.collect() === Array( (1,2),(2,1)) )
     assert( pajek.sparseMat.collect() === Array( (1,(2,2)) ) )
-  }
-
-  test("Read Nets/kin.net") {
-    val pajek = new PajekFile(sc,"Nets/kin.net")
-    assert( pajek.n === 20 )
-    assert( pajek.names.collect()(7) === (8,"f08") )
-    assert( pajek.weights.collect()(16) === (17,0.5) )
-    val correctEntries =
-      Array(
-        (6,(3,1)),
-        (6,(7,1)),
-        (6,(8,1)),
-        (6,(9,1)),
-        (9,(1,1)),
-        (9,(16,1)),
-        (11,(1,1)),
-        (11,(12,1)),
-        (13,(2,1)),
-        (13,(14,1)),
-        (13,(15,1)),
-        (16,(1,1)),
-        (16,(17,1)),
-        (19,(1,1)),
-        (19,(20,1))
-      )
-    assert( pajek.sparseMat.collect.sorted === correctEntries )
   }
 
   /***************************************************************************
