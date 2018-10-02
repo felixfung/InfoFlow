f71f7ef9a5732f2bc6f67e03ee5828142d96a138
Felix Fung <fylixeoi@gmail.com>
2018-10-01 23:53:53 -0400

    Added (simple) class Graph

---
+++ src/main/scala/graph.scala
@@ -0,0 +1,9 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD
+
+sealed case class Graph
+(
+  vertices: RDD[(Long,(String,Long))], // | index , name , module |
+  edgesWeight: RDD[(Long,(Long,Double))] // | index from , index to , weight |
+)
