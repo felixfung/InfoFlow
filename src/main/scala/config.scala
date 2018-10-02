e29d7d2b761fe3773f6d6f17469772b5405a048c
Felix Fung <fylixeoi@gmail.com>
2018-10-01 23:26:02 -0400

    Revert "Refactored ConfigFile" (should be in branch refactor)
    This reverts commit d40e1985ded752d8ad259b3e5f62e218c9e479ba.

--- src/main/scala/config.scala
+++ src/main/scala/config.scala
@@ -1,52 +1,55 @@
+import java.io.FileReader
+import java.io.BufferedReader
+
+class Config( fileName: String ) {
   /***************************************************************************
-   * class to read in config file
-   * class that reads in a Json config file
-   * usage: val configFile = new ConfigFile("config.json")
-   *        val master = configFile.master
-   *        and so on to access other properties
+   * Object to read config file
    ***************************************************************************/
 
-import scala.util.parsing.json._
+  val(
+    master, pajekFile, mergeAlgo, dampingFactor,
+    logDir, logWriteLog, rddText, rddJSon, logSteps ):
+  (String, String,String,Double,String,Boolean,Boolean,Int,Boolean) = {
 
-sealed case class ConfigFile
-(
-  master: String,
-  graphFile: String,
-  algorithm: String,
-  tele: Double,
-  logFile: ConfigFile.LogParams
-)
+  /***************************************************************************
+   * read and parse JSon file content
+   ***************************************************************************/
+    // read JSon content
+    val jsonContent = {
+      val reader = new BufferedReader( new FileReader(fileName) )
+      var line = reader.readLine
+      var wholefile = line
+      var looping = true
+      while(looping) {
+        line = reader.readLine
+        looping = ( line != null )
+        if(looping)
+          wholefile += line
+      }
+      reader.close
+      wholefile
+    }
 
-object ConfigFile
-{
-  // class that holds parameters for log file
-  // used in ConfigFile class
-  sealed case class LogParams(
-    val pathLog:       String,   // plain text file path for merge progress data
-    val pathParquet:   String,   // parquet file path for graph data
-    val pathRDD:       String,   // RDD text file path for graph data
-    val pathJson:      String,   // local Json file path for graph data
-    val savePartition: Boolean,  // whether to save partitioning data
-    val saveName:      Boolean,  // whether to save node naming data
-    val debug:         Boolean   // whether to print debug details
-  )
+    // parse JSon content
+    val json = scala.util.parsing.json.JSON.parseFull(jsonContent)
+    .asInstanceOf[Option[Map[String,String]]]
 
-  def apply( filename: String ): ConfigFile = {
-    val rawJson = new JsonReader(filename)
-    ConfigFile(
-      rawJson.getVal("Master").toString,
-      rawJson.getVal("Graph").toString,
-      rawJson.getVal("Algo").toString,
-      rawJson.getVal("tele").toString.toDouble,
-      ConfigFile.LogParams(
-        rawJson.getVal("log","log path").toString,
-        rawJson.getVal("log","Parquet path").toString,
-        rawJson.getVal("log","RDD path").toString,
-        rawJson.getVal("log","Json path").toString,
-        rawJson.getVal("log","save partition").toString.toBoolean,
-        rawJson.getVal("log","save name").toString.toBoolean,
-        rawJson.getVal("log","debug").toString.toBoolean
-      )
+  /***************************************************************************
+   * grab data
+   ***************************************************************************/
+    val master = json.map(_("Master")).getOrElse("local[*]")
+    val pajekFile = json.map(_("Pajek")).getOrElse("pajek.net")
+    val mergeAlgo = json.map(_("Algo")).getOrElse("InfoFlow")
+    val dampingFactor = json.map(_("damping")).getOrElse("0.85").toDouble
+    val logDir = json.map(_("logDir")).getOrElse(".")
+    val logWriteLog = json.map(_("logWriteLog")).getOrElse("false").toBoolean
+    val rddText = json.map(_("logRddText")).getOrElse("false").toBoolean
+    val rddJSon = json.map(_("logRddJSon")).getOrElse("1").toInt
+    val logSteps = json.map(_("logSteps")).getOrElse("false").toBoolean
+
+    (
+      master, pajekFile, mergeAlgo, dampingFactor,
+      logDir, logWriteLog, rddText, rddJSon, logSteps
     )
   }
 }
