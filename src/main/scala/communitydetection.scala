15a3163a4b89466f334226fb81d200a5b97147d4
Felix Fung <fylixeoi@gmail.com>
2018-10-27 01:47:45 -0400

    Removed redundant checking of strictly positive quantities in CommunityDetection.calCodelength()

--- src/main/scala/communitydetection.scala
+++ src/main/scala/communitydetection.scala
@@ -46,13 +46,11 @@ object CommunityDetection {
   ) = {
     val q12 = calQ( network.nodeNumber, n1+n2, p1+p2, network.tele, w12 )
     if( q12 > 0 ) {
-      if( qi_sum>0 && qi_sum+q12-q1-q2>0 ) (
+      (
         +plogp( qi_sum +q12-q1-q2 ) -plogp(qi_sum)
         -2*plogp(q12) +2*plogp(q1) +2*plogp(q2)
         +plogp(p1+p2+q12) -plogp(p1+q1) -plogp(p2+q2)
       )
-      else
-        throw new Exception("caught some crap: log(0)")
     }
     else {
       //throw new Exception("caught some crap: one giant module")
