7fe5de51a135b079896f3e46de3a1d27b31cece9
Felix Fung <fylixeoi@gmail.com>
2018-12-14 14:45:49 -0500

    PajekReader parses edge specifications with extra characters after edge weights

--- src/main/scala/pajekreader.scala
+++ src/main/scala/pajekreader.scala
@@ -28,8 +28,8 @@ object PajekReader
       val starRegex = """\*([a-zA-Z]+).*""".r
       val verticesRegex = """(?i)\*Vertices[ \t]+([0-9]+)""".r
       val vertexRegex = """[ \t]*?([0-9]+)[ \t]+\"(.*)\".*""".r
-      val edgeRegex1 = """[ \t]*?([0-9]+)[ \t]+([0-9]*)[ \t]*""".r
-      val edgeRegex2 = """[ \t]*?([0-9]+)[ \t]+([0-9]*)[ \t]+([0-9.]+)[ \t]*""".r
+      val edgeRegex1 = """[ \t]*?([0-9]+)[ \t]+([0-9]+)[ \t]*""".r
+      val edgeRegex2 = """[ \t]*?([0-9]+)[ \t]+([0-9]+)[ \t]+([0-9.]+).*""".r
 
       // store sectioning of file
       // defaults as "__begin"
