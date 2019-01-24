533ef3044f40c2cb4a81399bbb461adb17b79edd
Felix Fung <fylixeoi@gmail.com>
2019-01-24 14:55:09 -0500

    Refactored JsonReader so that it returns a Json object from each query

--- src/main/scala/jsonreader.scala
+++ src/main/scala/jsonreader.scala
@@ -1,30 +1,64 @@
 /*****************************************************************************
  * class to read Json file
  * using Scala standard library backend
- * return value is always Any
+ * return value is always a custom class, JsonObj
  * and up to user to convert
  *
  * usage:
  *   val jsonReader = new JsonReader(filename)
- *   val value = jsonReader.getVal("key")
- *   val nestedValue = jsonReader.getVal("object key","key")
- *   val moreNesting = jsonReader.getVal("more nesting","object key","key")
- *   and so on
+ *   val value = jsonReader.getObj(["key"]).value.toString
+ *   val nestedValue = jsonReader.getObj(["object key","key"]).value.toDouble
+ *   val moreNesting = jsonReader.getObj
+ *                       (["more nesting","object key","key"]).value.toBoolean
+ * and so on for final values
+ * nested objects can be accessed similarly:
+ *   val obj = jsonReader.getObj(["object key"])
+ *   val nestedValue = obj.getObj(["key"])
+ *   val actualValue = nestedValue.value.toDouble
+ * and similarly for higher nested objects
  *
- * if requesting an object rather than a value
- * then the returned value is a mapping (if user cast from Any)
  *****************************************************************************/
 
 import scala.util.parsing.json._
 
 sealed class JsonReader( filename: String )
 {
+/*****************************************************************************
+ * strategy is to read in whole file
+ * then wrap it into a JsonObj, which can then be accessed
+ *****************************************************************************/
+  sealed case class JsonObj( value: Any ) {
+    def getObj( keys: Seq[String] ): JsonObj = {
+      try {
+        keys match {
+          case key::Nil =>
+            JsonObj( value.asInstanceOf[Map[String,Any]] (key) )
+          case key :: nextkeys => {
+            val nextObj = JsonObj(
+              value.asInstanceOf[Map[String,Any]] (key) )
+            nextObj.getObj(nextkeys)
+          }
+	    }
+	  }
+      catch {
+        case e: java.lang.ClassCastException =>  throw e
+        case e: Throwable => throw(e)
+      }
+    }
+  }
+
+/*****************************************************************************
+ * strategy is to read in whole file
+ * then wrap it into a JsonObj, which can then be accessed
+ *****************************************************************************/
+  // read in whole file
   val wholeFile: String = {
     val source = scala.io.Source.fromFile(filename)
     try source.mkString
     finally source.close
   }
 
+  // check Json file validity
   {
     val tryParse = JSON.parseFull(wholeFile)
     tryParse match {
@@ -33,16 +67,6 @@ sealed class JsonReader( filename: String )
     }
   }
 
-  def getVal( keys: String* ): Any = {
-    var parsedJson: Any = JSON.parseFull(wholeFile).get
-    try {
-      for( key <- keys )
-        parsedJson = parsedJson.asInstanceOf[Map[String,Any]] (key)
-    }
-    catch {
-      case e: java.lang.ClassCastException =>  throw e
-      case e: Throwable => throw(e)
-    }
-    parsedJson
-  }
+  private val jsonObj = JsonObj( JSON.parseFull(wholeFile).get )
+  def getObj( keys: Seq[String] ): JsonObj = jsonObj.getObj(keys)
 }
